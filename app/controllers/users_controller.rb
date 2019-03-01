fc4461a716237262a4655447bd2de46f02042450
takanamir <rtakanami0706@gmail.com>
2019-03-01 15:03:09 +0000

    Fix Show Likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @micropost = Micropost.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
@@ -30,21 +29,18 @@ class UsersController < ApplicationController
   
   def followings
     @user = User.find(params[:id])
-    @micropost = Micropost.find(params[:id])
     @followings = @user.followings.page(params[:page])
     counts(@user)
   end
   
   def followers
     @user = User.find(params[:id])
-    @micropost = Micropost.find(params[:id])
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
   
   def likes
     @user = User.find(params[:id])
-    @micropost = Micropost.find(params[:id])
     @likes = @user.likes.page(params[:page])
     counts(@user)
   end
